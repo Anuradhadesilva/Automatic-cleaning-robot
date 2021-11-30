d9e252faf14336afb94092517782ac0d1524054b
Anuradha Desilva <94860426+Anuradhadesilva@users.noreply.github.com>
2021-11-30 21:52:38 +0530

    motor drive

---
+++ driver.h
@@ -0,0 +1,30 @@
+/***********************************************************************
+  
+            https://github.com/VinodLiyanage/GLASS-CLEANER-ROBOT
+  -------------------------------- (C) ---------------------------------
+  
+  @name driver.h
+  @version 1.0.0
+  @author Vinod Liyanage
+  @description Control Motor Functionalities
+
+************************************************************************/
+
+
+#ifndef DRIVER_H_
+#define DRIVER_H_
+
+void driver_init(
+unsigned char _in1,
+unsigned char _in2,
+unsigned char _in3,
+unsigned char _in4,
+unsigned char _ena,
+unsigned char _enb
+);
+
+void driver_go(unsigned char direction, unsigned char speed);
+void driver_rotate(unsigned char direction, unsigned char speed);
+void driver_stop(unsigned char motor_l, unsigned char motor_r);
+
+#endif /* DRIVER_H_ */
