0d0954cb9fb612bb0d64c8c2bde1156d93f151d1
Anuradha Desilva <94860426+Anuradhadesilva@users.noreply.github.com>
2022-06-07 00:31:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,52 +1,62 @@
 /*
- * dc motor new.c
+ * GccApplication9.c
  *
- * Created: 12/6/2021 10:24:45 AM
+ * Created: 5/8/2022 9:59:57 AM
  * Author : Anuradha
  */ 
-#define F_CPU 8000000UL
+
+#define F_CPU 8000000L
 #include "avr/io.h"
 #include <util/delay.h>
 
-void PWM_init()
-{
+
+void main(){
 	
-	/*set fast PWM mode with non-inverted output*/
-	TCCR1B = (1<<WGM00) | (1<<WGM01) | (1<<COM01) | (1<<CS00);
-	TCCR1A = (1<<WGM00) | (1<<WGM01) | (1<<COM01) | (1<<CS00);
-	DDRD|=(1<<PD4);  /*set OCR1B pin as output*/
+	DDRC=0xFF;
+	
+	motor_go('f');
+	_delay_ms(3000);
+	
+	motor_rotate('r');
+	_delay_ms(3000);
+	
+	motor_rotate('l');
+	_delay_ms(3000);
+	
+	motor_stop(0);
+	
+	
 }
 
 
-int main ()
-{
-	unsigned char duty;
-	
-	PWM_init();
-	DDRC=0xFF;
-	PORTC|=(1<<PC3|1<<PC5);
-	PORTC&=(~(1<<PC2|1<<PC4));
+void motor_go(unsigned char dir){
 	
 	
-		for(duty=0; duty<255; duty++)
-		{
-			OCR1B=duty;  
-			_delay_ms(3);
-		}
-		_delay_ms(2000);
-		for(duty=255; duty>1; duty--)
-		{
-			OCR1B=duty;  
-			_delay_ms(3);
-		}
-		_delay_ms(80);
-		PORTC&=(~(1<<PC2|1<<PC5));
+	if(dir=='f'){
+		PORTC|=(1<<PC3|1<<PC5);
+		PORTC&=(~(1<<PC2|1<<PC4));
+	}
+	
+	
+	
+}
+void motor_rotate(unsigned char dir){
+	
+	
+	if (dir=='r'){
 		PORTC|=(1<<PC3|1<<PC4);
-		for(duty=0; duty<100; duty++)
-		{
-			OCR1B=duty;  
-			_delay_ms(1);
-		}
-		_delay_ms(4000);
-		
+		PORTC&=(~(1<<PC2|1<<PC5));
+	}
+	
+	else if (dir== 'l'){
+		PORTC|=(1<<PC2|1<<PC5);
+		PORTC&=(~(1<<PC3|1<<PC4));
+	}
+}
+void motor_stop(unsigned char dir){
+	
+	if(dir==0){
+	PORTC&=(~(1<<PC2)&~(1<<PC3));
+	PORTC&=(~(1<<PC4)&~(1<<PC5));
+	}
 }
